d8875fea5da7359dfd26b4587006565ed8d598e4
Scott Sun <minghuisunscott@gmail.com>
2022-12-10 14:23:09 -0500

    archive scratches, add r doc to functions

--- simple_linear.R
+++ simple_linear.R
@@ -1,7 +1,14 @@
 library(tidyverse)
 library(progress)
 
-pseduo_x <- function(params, x, y, m) {
+#' calculate E(x) of f(x|y, x_obs)
+#' 
+#' @param params
+#' @param x
+#' @param y
+#' @param m
+#' @return a vector of "pseudo" expected value of x
+pseudo_x <- function(params, x, y, m) {
   # mask NA with an arbitrary value
   x <- replace_na(x, -999)
   # decompose params
@@ -16,14 +23,21 @@ pseduo_x <- function(params, x, y, m) {
 }
 
 .pseudo_sum_x <- function(params, x, y, m, likelihood_mode) {
-  pseduo_x_ <- pseduo_x(params, x, y, m) 
+  pseudo_x_ <- pseudo_x(params, x, y, m) 
   switch (likelihood_mode,
-    "full" = return(sum(pseduo_x_)),
-    "observed" = return(sum(m * pseduo_x_))
+    "full" = return(sum(pseudo_x_)),
+    "observed" = return(sum(m * pseudo_x_))
   )
 }
 
-pseduo_x_sq <- function(params, x, y, m) {
+#' calculate E(x^2) of f(x|y, x_obs)
+#' 
+#' @param params
+#' @param x
+#' @param y
+#' @param m
+#' @return a vector of "pseudo" expected value of x^2
+pseudo_x_sq <- function(params, x, y, m) {
   # mask NA with an arbitrary value
   x <- replace_na(x, -999)
   # decompose params
@@ -38,15 +52,22 @@ pseduo_x_sq <- function(params, x, y, m) {
   return(m * x^2 + (1 - m) * (ksi^2 + phi_sq))
 }
 
-.pseduo_sum_x_sq <- function(params, x, y, m, likelihood_mode) {
-  pseduo_x_sq_ <- pseduo_x_sq(params, x, y, m) 
+.pseudo_sum_x_sq <- function(params, x, y, m, likelihood_mode) {
+  pseudo_x_sq_ <- pseudo_x_sq(params, x, y, m) 
   switch (likelihood_mode,
-          "full" = return(sum(pseduo_x_sq_)),
-          "observed" = return(sum(m * pseduo_x_sq_))
+          "full" = return(sum(pseudo_x_sq_)),
+          "observed" = return(sum(m * pseudo_x_sq_))
   )
 }
 
-
+#' Maximization step in EM algorithm
+#' 
+#' @param params, initial guess on the estimates
+#' @param x
+#' @param y
+#' @param m
+#' @param likelhood_mode: optimization over "full" or "observed" likelihood
+#' @return maximized parameter point estimates in one iteration
 m_step <- function(params, x, y, m, likelihood_mode) {
   mu_1 <- params$mu_1
   sigma_1_sq <- params$sigma_1_sq
@@ -55,10 +76,10 @@ m_step <- function(params, x, y, m, likelihood_mode) {
   b2 <- params$b2
   
   A <- .pseudo_sum_x(params, x, y, m, "full")
-  B <- .pseduo_sum_x_sq(params, x, y, m, "full")
+  B <- .pseudo_sum_x_sq(params, x, y, m, "full")
   A.observed <- .pseudo_sum_x(params, x, y, m, "observed")
-  B.observed <- .pseduo_sum_x_sq(params, x, y, m, "observed")
-  C <- sum(pseduo_x(params, x, y, m) * y)
+  B.observed <- .pseudo_sum_x_sq(params, x, y, m, "observed")
+  C <- sum(pseudo_x(params, x, y, m) * y)
   
   n <- length(m)
   if (likelihood_mode == "full") {
@@ -84,6 +105,16 @@ m_step <- function(params, x, y, m, likelihood_mode) {
   ))
 }
 
+#' EM algorithm
+#' 
+#' @param params
+#' @param x
+#' @param y
+#' @param m
+#' @param likelhood_mode, optimization over "full" or "observed" likelihood
+#' @param maxit
+#' @param tol
+#' @return maximized parameter point estimates after a convergence is met
 em <- function(params, x, y, m, likelihood_mode, maxit = 50, tol = 1e-5) {
   # iterative estimation
   for (i in seq_len(maxit)) {
@@ -98,6 +129,14 @@ em <- function(params, x, y, m, likelihood_mode, maxit = 50, tol = 1e-5) {
   return(params)
 }
 
+#' EM algorithm variance-covariance matrix through (n * Cov(s(Θ)))^{-1}
+#' 
+#' @param params, optimized estimates
+#' @param x
+#' @param y
+#' @param m
+#' @param likelhood_mode: optimization over "full" or "observed" likelihood
+#' @return variance-covariance matrix
 em_vcov <- function(params, x, y, m) {
   # decompose params
   mu_1 <- params$mu_1
@@ -106,26 +145,33 @@ em_vcov <- function(params, x, y, m) {
   b1 <- params$b1
   b2 <- params$b2
   # augmented data
-  pseduo_x_ <- pseduo_x(params, x, y, m)
-  pseduo_x_sq_ <- pseduo_x_sq(params, x, y, m)
+  pseudo_x_ <- pseudo_x(params, x, y, m)
+  pseudo_x_sq_ <- pseudo_x_sq(params, x, y, m)
   # inference: score
-  fs_b1 <- 1 / sigma_sq * (y - b1 - b2 * pseduo_x_)
+  fs_b1 <- 1 / sigma_sq * (y - b1 - b2 * pseudo_x_)
   fs_b2 <-
-    1 / sigma_sq * (y * pseduo_x_ - b1 * pseduo_x_ - b2 * pseduo_x_sq_)
+    1 / sigma_sq * (y * pseudo_x_ - b1 * pseudo_x_ - b2 * pseudo_x_sq_)
   fs_sigma_sq <-
-    -1 / (2 * sigma_sq) + 1 / (2 * sigma_sq ^ 2) * (y ^ 2 - 2 * b1 * y - 2 * b2 * y * pseduo_x_ +
-                                                      b1 ^ 2 + 2 * b1 * b2 * pseduo_x_ + b2 ^ 2 * pseduo_x_sq_)
-  fs_mu_1 <- 1 / sigma_1_sq * (pseduo_x_ - mu_1)
+    -1 / (2 * sigma_sq) + 1 / (2 * sigma_sq ^ 2) * (y ^ 2 - 2 * b1 * y - 2 * b2 * y * pseudo_x_ +
+                                                      b1 ^ 2 + 2 * b1 * b2 * pseudo_x_ + b2 ^ 2 * pseudo_x_sq_)
+  fs_mu_1 <- 1 / sigma_1_sq * (pseudo_x_ - mu_1)
   fs_sigma_1_sq <-
-    -1 / (2 * sigma_1_sq) + 1 / (2 * sigma_1_sq ^ 2) * (pseduo_x_sq_ - 2 * mu_1 * pseduo_x_ + mu_1 ^ 2)
+    -1 / (2 * sigma_1_sq) + 1 / (2 * sigma_1_sq ^ 2) * (pseudo_x_sq_ - 2 * mu_1 * pseudo_x_ + mu_1 ^ 2)
   score <- cbind(fs_mu_1, fs_sigma_1_sq, fs_sigma_sq, fs_b1, fs_b2)
   colnames(score) <- names(params)
-  # score -to-> fisher information
+  # score -> fisher information -> Cov
   n <- length(y)
-  fisher_info <- solve(n * var(score))
-  return(fisher_info)
+  cov_mat <- solve(n * var(score))
+  return(cov_mat)
 }
 
+#' Calculate theoretical 95% CI based on the SE from `em_vcov`
+#' 
+#' @param est_params, optimized parameter estimates
+#' @param x
+#' @param y
+#' @param m
+#' @return a tibble of theoretical 95% CI
 em_theoretical_ci <- function(est_params, x, y, m) {
   out <- est_params %>% as.data.frame() %>% gather()
   se <- sqrt(diag(em_vcov(est_params, x, y, m)))
@@ -136,6 +182,15 @@ em_theoretical_ci <- function(est_params, x, y, m) {
     tibble()
 }
 
+#' Implement bootstrapping for the EM algorithm and document estimates for each replicate
+#' 
+#' @param B, number of boot replicates
+#' @param params, common initial guess
+#' @param x
+#' @param y
+#' @param m
+#' @param ...
+#' @return estimates from all replicates
 em_bootstrapping <- function(B, params, x, y, m, ...) {
   pb <- progress_bar$new(
     format = "  downloading [:bar] :percent eta: :eta",
@@ -154,6 +209,10 @@ em_bootstrapping <- function(B, params, x, y, m, ...) {
   return(boot_ests)
 }
 
+#' Calculate bootstrapping 95% CI by aggregating the result
+#' 
+#' @param boot_ests, estimates from all boot replicates
+#' @return estimates from all replicates
 boot_confint <- function(boot_ests) {
   boot_ests %>% as.data.frame() %>% gather() %>% group_by(key) %>%
     summarise(lower_bd = quantile(value, 0.025),
